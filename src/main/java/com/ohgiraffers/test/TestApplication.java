0fca37e1525b4b3fcef7f4393320707898849f94
dongwan <eamdongwan@gmail.com>
2024-01-31 14:30:53 +0900

    [test] 프로그램 test

---
+++ src/main/java/com/ohgiraffers/test/TestApplication.java
@@ -0,0 +1,23 @@
+package com.ohgiraffers.test;
+
+public class TestApplication {
+// 수업목표 오늘의 수업 목표입니다.
+    // 여기는 주석입니다.
+    /* 여기는
+     * 여러 줄
+     * 주석 입니다.
+     * */
+
+    /*
+     * ctrl - space : 자동 완성 기능
+     * ctrl - d 복사
+     // () {} []
+     */
+
+    public static void main(String[] args) {
+        System.out.println("저는 개발자 입니다.");
+    }
+}
+
+
+
